2f8c38280e5287ad09c903a382782aa3355c640f
bug3 <bug3dev@gmail.com>
2022-03-28 21:16:47 +0300

    Define cli

--- src/main.py
+++ src/main.py
@@ -2,3 +2,11 @@ import click
 import json
 from os.path import dirname, realpath
 from subprocess import getstatusoutput, run
+
+
+@click.command()
+@click.option("--event", default="on-change", help="Event")
+@click.option("--script", default="run", help="The script to run when the event occurs")
+def cli(event, script):
+    """This is description"""
+    click.echo(f"event={event} script={script}")
