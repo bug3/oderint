a702e7cc9a2601fc1e528acc0829e328c5c73859
bug3 <bug3dev@gmail.com>
2022-03-28 21:26:53 +0300

    Setup v0.0.0

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="oderint",
+    version="0.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Click",
+    ],
+    entry_points={
+        "console_scripts": [
+            "oderint = src.main:test",
+        ],
+    },
+)
