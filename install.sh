aa05869c1d1286c01ace35926d904fe401bd39bd
bug3 <bug3dev@gmail.com>
2022-03-28 23:24:38 +0300

    Check app exists

--- install.sh
+++ install.sh
@@ -5,6 +5,19 @@ path="$HOME/.$app"
 dirs=("data" "events" "scripts" "src")
 files=("setup.py")
 
+install () {
+    if [[ ! -e $path ]]; then
+        mkdir $path
+    else
+        echo "$path already exists"
+        exit 1
+    fi
+}
+
+uninstall () {
+    rm -r $path
+}
+
 if [ $# -eq 0 ]; then
 	install
 elif [ $1 == "-r" ]; then
