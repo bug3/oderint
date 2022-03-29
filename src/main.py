8cb1aa72bae9a93c5999ae0b306722defeafd08a
bug3 <bug3dev@gmail.com>
2022-03-29 17:32:20 +0300

    Remove test func

--- src/main.py
+++ src/main.py
@@ -8,20 +8,10 @@ from subprocess import getstatusoutput, run
 @click.argument("file", type=click.Path(exists=True))
 @click.option("--event", default="on-change", help="Event")
 @click.option("--script", default="run", help="The script to run when the event occurs")
-def cli(event, script):
+def cli(file, event, script):
     """This is description"""
     click.echo(f"event={event} script={script}")
 
 
-def test():
-    path = dirname(dirname(realpath(__file__)))
-    status, output = getstatusoutput(path + "/events/on-change")
-
-    if status == 0:
-        run(path + "/scripts/run")
-    else:
-        print(output)
-
-
 if __name__ == "__main__":
-    test()
+    cli()
