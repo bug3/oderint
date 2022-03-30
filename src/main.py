2798f30a71a3c7a5cb2b09956495be3417e749c3
bug3 <bug3dev@gmail.com>
2022-03-30 17:36:53 +0300

    Make global path var

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from os.path import exists, dirname, realpath
 from subprocess import getstatusoutput, run
 
 
+path = dirname(dirname(realpath(__file__)))
+
+
 @click.command()
 @click.argument("file", type=click.Path(exists=True))
 @click.option("--event", default="on-change", help="Event")
@@ -16,18 +19,18 @@ def cli(file, event, script):
 
 
 def checkParams(event, script):
-    if exists("events/" + event) == False:
+    if exists(path + "/events/" + event) == False:
         raise click.UsageError(
             f"Invalid value for '--event': Path 'events/{event}' does not exist."
         )
-    if exists("scripts/" + script) == False:
+    if exists(path + "/scripts/" + script) == False:
         raise click.UsageError(
             f"Invalid value for '--script': Path 'scripts/{script}' does not exist."
         )
 
 
 def updateJson(b, u, g):
-    with open("data/oderint.json", "r+") as config:
+    with open(path + "/data/oderint.json", "r+") as config:
         data = json.load(config)
 
         data["file"] = b
@@ -40,8 +43,6 @@ def updateJson(b, u, g):
 
 
 def watch(event, script):
-    path = dirname(dirname(realpath(__file__)))
-
     while True:
         status, output = getstatusoutput(path + "/events/" + event)
 
