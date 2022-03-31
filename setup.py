b11ae21c08a5795121268232aa7f379bdf191696
bug3 <bug3dev@gmail.com>
2022-03-31 04:34:16 +0300

    Prepare __init__.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="oderint",
     version="0.0.0",
-    packages=find_packages(),
-    include_package_data=True,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
         "Click",
     ],
     entry_points={
         "console_scripts": [
-            "oderint = src.main:cli",
+            "oderint = oderint:cli",
         ],
     },
 )
