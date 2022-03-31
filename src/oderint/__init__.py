6f75beafcf9a3f6bf8634f5348771843d8049b88
bug3 <bug3dev@gmail.com>
2022-03-31 05:06:45 +0300

    Remove path var

--- src/oderint/__init__.py
+++ src/oderint/__init__.py
@@ -5,33 +5,34 @@ from os.path import exists, dirname, realpath
 from subprocess import getstatusoutput, run
 
 
-path = dirname(dirname(realpath(__file__)))
-
-
 @click.command()
 @click.argument("file", type=click.Path(exists=True))
 @click.option("--event", default="on-change", help="Event")
 @click.option("--script", default="run", help="The script to run when the event occurs")
 def cli(file, event, script):
     """This is description"""
+
+    fullpath = realpath(file)
+
+    chdir(dirname(dirname(dirname(realpath(__file__)))))
     checkParams(event, script)
-    updateJson(realpath(file), event, script)
+    updateJson(fullpath, event, script)
     watch(event, script)
 
 
 def checkParams(event, script):
-    if exists(path + "/events/" + event) == False:
+    if exists("events/" + event) == False:
         raise click.UsageError(
             f"Invalid value for '--event': Path 'events/{event}' does not exist."
         )
-    if exists(path + "/scripts/" + script) == False:
+    if exists("scripts/" + script) == False:
         raise click.UsageError(
             f"Invalid value for '--script': Path 'scripts/{script}' does not exist."
         )
 
 
 def updateJson(b, u, g):
-    with open(path + "/data/oderint.json", "r+") as config:
+    with open("data/oderint.json", "r+") as config:
         data = json.load(config)
 
         data["file"] = b
@@ -44,13 +45,11 @@ def updateJson(b, u, g):
 
 
 def watch(event, script):
-    chdir(path)
-
     while True:
-        status, output = getstatusoutput(path + "/events/" + event)
+        status, output = getstatusoutput("events/" + event)
 
         if status == 0:
-            run(path + "/scripts/" + script)
+            run("scripts/" + script)
         else:
             raise click.ClickException(output)
 
