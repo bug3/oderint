99bc5b14cb6e1ed1222d55490f2d04507a08e10a
bug3 <bug3dev@gmail.com>
2022-04-03 03:39:27 +0300

    Define removeText func

--- data/append-remove.sh
+++ data/append-remove.sh
@@ -17,3 +17,15 @@ appendText () {
         cat $textFile >> $destFile
     fi    
 }
+
+removeText () {
+    wordCount=$(grep -o "$(cat $textFile)" $destFile | wc -l)
+    isTextExists=$([[ $wordCount -ne 0 ]] && echo true || echo false)
+
+    if $isTextExists; then
+        diff $destFile $textFile | sed -n 's/^< //p' > $tempFile
+        lineCount=$(cat $tempFile | wc -l)
+        echo "$(head -$lineCount $tempFile)" > $destFile
+        rm $tempFile
+    fi
+}
