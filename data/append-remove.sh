327012bf87d8ebc5fdc64bb061ba6f74a289c1ab
bug3 <bug3dev@gmail.com>
2022-04-03 03:39:13 +0300

    Define appendText func

--- data/append-remove.sh
+++ data/append-remove.sh
@@ -5,3 +5,15 @@ textFile=$2
 tempFile=.vimrc-oderint.tmp
 
 cp $destFile $destFile.bak
+
+appendText () {
+    wordCount=$([[ -f $destFile ]] && cat $destFile | wc -w)
+
+    ed -s $destFile <<< w
+    
+    if [[ $wordCount -eq 0 ]]; then
+        tail -n +2 $textFile > $destFile
+    else
+        cat $textFile >> $destFile
+    fi    
+}
