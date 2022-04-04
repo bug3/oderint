756bb46251ace358d73ede0d5528217d94218337
bug3 <bug3dev@gmail.com>
2022-04-05 00:05:44 +0300

    Add author info

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="oderint",
     version="0.0.0",
     description="The execution assistant",
+    author="bug3",
+    author_email="bug3dev@gmail.com",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
