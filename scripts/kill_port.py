1acd805ebcfb831962da4119ce97be55829c158d
bug3 <bug3dev@gmail.com>
2022-04-05 03:28:31 +0300

    Define getPid func

--- scripts/kill_port.py
+++ scripts/kill_port.py
@@ -1,3 +1,9 @@
 from data.config import port
 from psutil import process_iter, net_connections
 from signal import SIGKILL
+
+
+def getPid(p):
+    for nc in net_connections():
+        if nc.laddr[1] == p:
+            return nc.pid
