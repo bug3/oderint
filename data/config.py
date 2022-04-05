70e647348e9be76034e4dba033657b46a6bc5326
bug3 <bug3dev@gmail.com>
2022-04-05 03:17:46 +0300

    Define config vars

--- data/config.py
+++ data/config.py
@@ -4,3 +4,8 @@ from os.path import dirname, basename, splitext
 f = open("data/oderint.json")
 config = json.load(f)
 f.close()
+
+path = config["path"]
+event = config["event"]
+script = config["script"]
+port = config["port"]
