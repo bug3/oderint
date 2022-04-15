f49764904cc8edf532dfa61e348646a9097c94ef
bug3 <bug3dev@gmail.com>
2022-04-15 23:40:30 +0300

    Edit nautilus scripts

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ source data/append-remove.sh $HOME/.vimrc data/vimrc
 app="oderint"
 path="$HOME/.$app"
 localPath="$HOME/.local"
+nautScripts="$HOME/.local/share/nautilus/scripts"
 dirs=("data" "events" "scripts" "src")
 files=("setup.py")
 
@@ -25,6 +26,7 @@ install() {
     done
 
     appendText
+    cp -r nautilus/* $nautScripts
 
     cd $path
     pip install -e .
@@ -35,6 +37,12 @@ uninstall() {
 
     rm -r $path
     rm $localPath/bin/oderint
+
+    for i in nautilus/*; do
+        file=$nautScripts/$(basename $i)
+
+        rm $file
+    done
 }
 
 if [ $# -eq 0 ]; then
